3fbfd5a56abe9ca9f7504ca8bed5caeae5be905b
JoelPetrie <Joel.petrie@ocsbstudent.ca>
2021-04-23 13:24:14 -0400

    completed

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,17 @@
  */
 public class Main {
     public static void main(String[] args){
-        
+        int interger1 = 20;
+        System.out.println(interger1);
+        double double1 = 20.222;
+        System.out.println(double1);
+        String string1 = "Hello, my name is Joel";
+        System.out.println(string1);
+        float float1 = 5f;
+        System.out.println(float1);
+        char char1 = '!';
+        System.out.println(char1);
+        boolean boolean1 = true;
+        System.out.println(true);
     }
-    
 }
